99e8857f1ae960c9124b8c6c859c1b77d019516a
David Redondo Durand <alu0100851700@ull.edu.es>
2016-09-14 17:51:56 +0100

    fix #9, fix #18. Se puede lanzar como demonio y sin el

--- server/main.cpp
+++ server/main.cpp
@@ -6,7 +6,23 @@
 #include <QFileInfo>
 #include <QDir>
 
+#include <iostream>
+#include <cmath>
+#include <cerrno>
+#include <cstring>
+#include <clocale>
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
+#include <fstream>
+
 #include "server.h"
+
+#include <syslog.h>
+
+#define O_RDONLY         00
+#define O_WRONLY         01
+#define O_RDWR           02
 
 bool fileExists(QString path) {
     QFileInfo check_file(path);
@@ -27,10 +43,16 @@ int main(int argc, char *argv[])
     QApplication::setApplicationName("Servidor Talk");
 
     int args;
+    bool daemon = true;
     QSettings settings;
 
-    while ( (args = getopt(argc, argv, "hp:k:c:01")) != -1) {
+    while ( (args = getopt(argc, argv, "thp:k:c:01")) != -1) {
         switch (args){
+            case 't':
+            {
+                daemon = false;
+                break;
+            }
             case 'p':
             {
                 settings.setValue("port", atoi(optarg));
@@ -59,7 +81,9 @@ int main(int argc, char *argv[])
                           << "  -k <key>\t.key file\n"
                           << "  -c <key>\t.crt file\n\n"
                           << "Server configuration\n"
-                          << "  -p <port>\tserver port"
+                          << "  -p <port>\tserver port\n"
+                          << "Terminal\n"
+                          << "  -t\t\tRun Server in terminal\n"
                           << std::endl;
                 return 0;
                 break;
@@ -72,6 +96,65 @@ int main(int argc, char *argv[])
             }
         }
     }
+
+    if (daemon){
+        pid_t pid;
+
+        // Nos clonamos a nosotros mismos creando un proceso hijo
+        pid = fork();
+
+        // Si pid es < 0, fork() falló
+        if (pid < 0) {
+            // Mostrar la descripción del error y terminar
+            std::cerr << std::strerror(errno) << '\n';
+            exit(10);
+        }
+
+        // Si pid es > 0, estamos en el proceso padre
+        if (pid > 0) {
+            // Terminar el proceso
+            exit(0);
+        }
+
+        // Si la ejecución llega a este punto, estamos en el hijo
+/*
+
+        openlog(argv[0], LOG_NOWAIT | LOG_PID, LOG_USER);
+
+        // Cambiar el directorio de trabajo de proceso
+        if ((chdir("/")) < 0) {
+            syslog(LOG_ERR, "No fue posible cambiar el directorio de trabajo a /\n");
+            exit(12);
+        }
+
+        pid_t sid;
+
+        // Intentar crear una nueva sesión
+        sid = setsid();
+        if (sid < 0) {
+            syslog(LOG_ERR, "No fue posible crear una nueva sesión\n");
+            exit(11);
+        }
+
+        // Cambiar el usuario efectivo del proceso a 'midemonio'
+        passwd* user = getpwnam("talkServer");
+        seteuid(user->pw_uid);
+
+        // Cambiar el grupo efectivo del proceso a 'midemonio'
+        group* group = getgrnam("talkServer");
+        setegid(group->gr_gid);
+*/
+        // Cerrar los descriptores de la E/S estándar
+        close(STDIN_FILENO);                    // fd 0
+        close(STDOUT_FILENO);                   // fd 1
+        close(STDERR_FILENO);                   // fd 2
+/*
+        int fd0 = open("/dev/null", O_RDONLY);  // fd0 == 0
+        int fd1 = open("/dev/null", O_WRONLY);  // fd0 == 1
+        int fd2 = open("/dev/null", O_WRONLY);  // fd0 == 2
+*/
+    }
+
     if (!fileExists(settings.value("sslkey").toString()))
         settings.setValue("sslkey", QDir::currentPath() + "/certificate/server.key");
     if (!fileExists(settings.value("sslkey").toString())){
